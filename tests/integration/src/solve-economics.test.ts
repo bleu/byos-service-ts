fdd8c9db128db65f2a88fc4c5a5bf8caa13e4e40
Pedro Yves Fracari <yves.fracari@gmail.com>
2026-08-26 18:58:12 -0300

    test(solve): make encoded route nonce explicit

--- tests/integration/src/solve-economics.test.ts
+++ tests/integration/src/solve-economics.test.ts
@@ -381,8 +381,9 @@ describe("/solve economics", () => {
 			quoteBuyAmount: 950n,
 			gasUsed: 200_000n,
 			trampoline: TRAMPOLINE,
-			interactions: route,
-			signature,
+		interactions: route,
+		signature,
+		nonce: 999n,
 		});
 
 		const { solutions } = await postSolve(auctionJson(u, "sell", "1000", "900", "0"));
@@ -402,7 +403,7 @@ describe("/solve economics", () => {
 				minBuyAmount: 950n,
 				quoteBuyAmount: 950n,
 				validUntil: 2n ** 40n,
-				nonce: 1n,
+				nonce: 999n,
 			},
 			route,
 			signature,
